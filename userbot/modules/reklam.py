56abd10a5e04ba154ec447e2b3470c2f2af465cc
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2021-08-20 00:19:58 +0300

    Update reklam.py

--- userbot/modules/reklam.py
+++ userbot/modules/reklam.py
@@ -1,17 +1,7 @@
-import logging
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.WARNING)
-from telethon import events
-from datetime import datetime
-from userbot.util import admin_cmd
-from userbot import bot
+from typing import Pattern
+from telethon import TelegramClient,events 
 
-
-@bot.on(admin_cmd(pattern="reklam"))
-async def _(event):
-    await event.delete()
-    for a in range(1,2):
-        await bot.send_message(
-            event.chat_id,
-            "REKLAM SEÇENEKLERİMİZ:\n=========================\n25K KANAL👇\n6 saat 40TL (3 saat ön plan sonra sabit)\n12 saat 60TL (5 saat ön plan sonra sabit)\n24 saat 90TL (10 saat ön plan sonra sabit)\n\n50K KANAL👇\n6 saat 80TL (3 saat ön plan sonra sabit)\n12 saat 150TL (5 saat ön plan sonra sabit)\n24 saat 250TL (10 saat ön plan sonra sabit)\n\n125K KİTLE👇\n6 saat 120TL (3 saat ön plan sonra sabit)\n12 saat 200TL (5 saat ön plan sonra sabit)\n24 saat 350TL (10 saat ön plan sonra sabit)\n========================="
-        )
+@bot.on(events.NewMessage(pattern='^(?i)kontrol$'))
+async def deneme (event):
+    global deneme_m
+    await event.reply("Çalışıyor...")
