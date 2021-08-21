9b5e3249ebeb2dfe4907283f769e88bb6f5b084f
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2021-08-22 01:23:13 +0300

    Update otopost.py

--- userbot/modules/otopost.py
+++ userbot/modules/otopost.py
@@ -9,6 +9,7 @@ id_list = [1275989066,1435523233,1276667828,1399376603,1377871517,1559899893,132
 async def otoreklamm(event):
     global id_list
     text = "SAĞLAM SİSTEMİMİZ İLE ASLA SİZİ ÜZMÜYORUZ..\n\nSizde tıklanmalarınız eksik sayılmasına engel olmak ve 22TL CPM ile kazancına kazanç katmak için yapman gereken tek şey PND.TL ye gelmek\n\n[👉PND.TL TELEGRAM GRUBUMUZ İÇİN TIKLA👈](https://t.me/joinchat/QXzIKZw5BV5mMmJk)"
+    await event.edit("Çalışıyor.")
     while True:
         for x in id_list:
             try:
@@ -16,5 +17,6 @@ async def otoreklamm(event):
             except:
                 await bot.send_message("me",f"{x} idyi kotrol et aq !")
                 pass
+        await bot.send_message("me","Gönderildi !")
         await asyncio.sleep(7200)
 
