b4ef028c0a8b986bb8abfb413fecc4216eb4bd23
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2021-08-22 14:08:44 +0300

    Create admingroup.py

---
+++ userbot/modules/admingroup.py
@@ -0,0 +1,20 @@
+import asyncio
+from typing import Tuple
+from userbot.events import register
+from userbot import bot
+
+
+id_list = [-1001275989066,-1001435523233,-1001276667828,-1001399376603,-1001377871517,-1001559899893,-1001328224261,-1001476898506,-1001254179689,-1001226168546,-1001245244239,-1001583615217,-1001463683383,-1001338215425,-1001224401851,-1001561556131]
+@register(outgoing=True, pattern="^.admin")
+async def otoreklamm(event):
+    global id_list
+    reply_message = await event.get_reply_message()
+    while True:
+        for x in id_list:
+            try:
+                await bot.send_file(int(x),reply_message) 
+            except Exception as e: 
+                print(e)
+                await bot.send_message("me",f"{x} idyi kotrol et aq !")
+                pass
+        await bot.send_message("me","Gönderildi !")
