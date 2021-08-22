cc812eb668bfbfa11a46bbd4323738b05641333d
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2021-08-22 03:10:05 +0300

    Update otopnd.py

--- userbot/modules/otopnd.py
+++ userbot/modules/otopnd.py
@@ -5,7 +5,7 @@ from userbot import bot
 
 
 id_list = [-1001275989066,-1001435523233,-1001276667828,-1001399376603,-1001377871517,-1001559899893,-1001328224261,-1001476898506,-1001254179689,-1001226168546,-1001245244239,-1001583615217,-1001463683383,-1001338215425,-1001224401851]
-@register(outgoing=True, pattern="^.otoreklam")
+@register(outgoing=True, pattern="^.otopnd")
 async def otoreklamm(event):
     global id_list
     text = "SAĞLAM SİSTEMİMİZ İLE ASLA SİZİ ÜZMÜYORUZ..\n\nSizde tıklanmalarınız eksik sayılmasına engel olmak ve 22TL CPM ile kazancına kazanç katmak için yapman gereken tek şey PND.TL ye gelmek\n\n[👉PND.TL TELEGRAM GRUBUMUZ İÇİN TIKLA👈](https://t.me/joinchat/QXzIKZw5BV5mMmJk)"
