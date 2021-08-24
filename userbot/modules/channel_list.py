6c5d96e1bf99eff8e07b60a0b0abbc8ad210d31d
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2021-08-24 17:56:36 +0300

    Update channel_list.py

--- userbot/modules/channel_list.py
+++ userbot/modules/channel_list.py
@@ -6,4 +6,4 @@ from userbot import bot
 
 @register(outgoing=True, pattern="^.kanallar")
 async def mesaj(event):
-    await event.edit("**Vip İfşa +18👇**\nhttps://t.me/joinchat/AAAAAEylXUB6ztFxdgHp1w\n\n**Terbiyesizmsnfbi**👇\nhttps://t.me/joinchat/AAAAAEv_bBx6bl0AEojIJA\n\n**+18 LİNK İFŞA**\nhttps://t.me/joinchat/V5uno4h5L43QKN9o**")
+    await event.edit("**Vip İfşa +18👇**\nhttps://t.me/joinchat/AAAAAEylXUB6ztFxdgHp1w\n\n**Terbiyesizmsnfbi**👇\nhttps://t.me/joinchat/AAAAAEv_bBx6bl0AEojIJA\n\n**+18 LİNK İFŞA**\nhttps://t.me/joinchat/V5uno4h5L43QKN9o")
