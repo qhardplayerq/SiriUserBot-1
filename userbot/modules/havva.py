2edbcc7dd668f0ed5f2168fd453355d1f490ebc9
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2021-08-27 21:14:55 +0300

    Create havva.py

---
+++ userbot/modules/havva.py
@@ -0,0 +1,21 @@
+import asyncio
+from typing import Tuple
+from userbot.events import register
+from userbot import bot
+
+
+idler = [-1001586245726,-1001052096636]
+
+@register(outgoing=True, pattern="^.gonder")
+async def deneme(event):
+    global idler
+    reply = await event.get_reply_message()
+    hata = 0
+    m = await event.respond(f"Toplu Gönderim Başladı !\n\nGönderilcek ID sayısı : {len(idler)}\nDurum : `Gönderiliyor...`")
+    for x in idler:
+            try:
+                await bot.send_message(x,reply.message)
+            except:
+                hata += 1
+                pass    
+    await bot.edit_message(event.chat.id,m,f"Toplu Gönderim Başladı !\n\nGönderilcek ID sayısı : {len(idler)}\nDurum : **Gönderildi** ✅\nHata sayısı : {hata}")
