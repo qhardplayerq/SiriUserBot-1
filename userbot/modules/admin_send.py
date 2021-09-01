be970c662fc9dbfe10571f37e4a86b7bfaabf93b
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-09-01 23:57:43 +0300

    update admin_send.py

--- userbot/modules/admin_send.py
+++ userbot/modules/admin_send.py
@@ -18,4 +18,4 @@ async def deneme(event):
             except:
                 hata += 1
                 pass
-    await bot.edit_message(event.chat.id,m,f"Toplu Gönderim Başladı !\n\nGönderilcek ID sayısı : {len(idler)}\nDurum : **Gönderildi** ✅\nHata sayısı : {hata}")
+    await m.edit(f"Toplu Gönderim Başladı !\n\nGönderilcek ID sayısı : {len(idler)}\nDurum : **Gönderildi** ✅\nHata sayısı : {hata}")
