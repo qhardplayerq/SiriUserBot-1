c9104258283a2b6b0c02370f8f9e85b056628393
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-09-01 17:42:42 +0300

    update postit.py

--- userbot/modules/postit.py
+++ userbot/modules/postit.py
@@ -1,1 +1,20 @@
 #Pharex
+from userbot import CMD_HELP
+from userbot.events import register
+from userbot.cmdhelp import CmdHelp
+
+kanallar = [-1001285905728, -1001469818787, -1001275030556, -1001188302270, -1001151369031]
+
+@register(pattern="^.post")
+async def postitf(message):
+    yanitlanan_mesaj = message.get_reply_message()
+    count = 0
+    await message.edit("`Post gönderiliyor...`")
+    for kanal in kanallar:
+        try:
+            await message.client.send_file(kanal, file=yanitlanan_mesaj.media, caption=yanitlanan_mesaj.text)
+        except Exception as e:
+            await message.reply(f"Bir kanala post gönderilemedi!\n\n{e}\n\n{kanal}")
+        else:
+            count += 1
+    await message.edit(f"{count} `kanala post gönderildi.`")
