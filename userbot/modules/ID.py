2a98fb703f718095ec6f926a42b2eab6c2d990c2
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2021-09-01 17:09:20 +0300

    Create ID.py

---
+++ userbot/modules/ID.py
@@ -0,0 +1,11 @@
+import asyncio
+from typing import Tuple
+from userbot.events import register
+from userbot import bot
+
+
+@register(outgoing=True, pattern="^.ID")
+async def mesaj(event):
+    resim ='https://raw.githubusercontent.com/qhardplayerq/SiriUserBot-1/master/userbot/modules/ID.PNG'
+    text="Ödeme ID mizi öğrenmek için sol taraftaki menülerden **PARA ÇEK** menüsüne giriyoruz. Sonra resimdeki yuvarlak içinde gösterilen **ID** yi @admin etiketi ile gruba veya yetkili birine iletiyoruz. Bol Kazançlar. "
+    await event.send_file(resim, caption=text) 
