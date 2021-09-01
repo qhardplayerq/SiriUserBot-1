ee53ecdf3596a57f544daf6415cef07b496fc422
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-09-02 01:32:42 +0300

    update admin_send.py

--- userbot/modules/admin_send.py
+++ userbot/modules/admin_send.py
@@ -17,6 +17,6 @@ async def deneme(event):
             await reply.forward(x)
         except Exception as e:
             hata += 1
-            await event.respond(str(e)
+            await event.respond(str(e))
         await m.edit(f"Toplu Gönderim Başladı !\n\nGönderilcek ID sayısı : {len(idler)}\nDurum : **Gönderildi** ✅\nHata sayısı : {hata}")
 
