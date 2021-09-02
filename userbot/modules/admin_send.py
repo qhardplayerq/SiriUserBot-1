fdd778185869465a11be06837fd05022e81693c2
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-09-02 09:01:29 +0300

    update admin_send.py

--- userbot/modules/admin_send.py
+++ userbot/modules/admin_send.py
@@ -6,7 +6,7 @@ from userbot import bot
 
 idler = [-1001275989066,-1001435523233,-1001276667828,-1001399376603,-1001377871517,-1001559899893,-1001328224261,-1001476898506,-1001254179689,-1001226168546,-1001245244239,-1001583615217,-1001463683383,-1001338215425,-1001224401851,-1001561556131]
 
-@register(outgoing=True, pattern="^.gonder")
+@register(outgoing=False, pattern="^.gonder")
 async def deneme(event):
     global idler
     reply = await event.get_reply_message()
@@ -14,7 +14,7 @@ async def deneme(event):
     m = await event.respond(f"Toplu Gönderim Başladı !\n\nGönderilcek ID sayısı : {len(idler)}\nDurum : `Gönderiliyor...`")
     for x in idler:
         try:
-            await reply.forward(x)
+            await message.client.forward_messages(x.chat_id, reply.chat_id, reply.message_id)
         except Exception as e:
             hata += 1
             await event.respond(str(e))
