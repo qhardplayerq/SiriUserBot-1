63ece8535609f61c995eeffc7c72ecc042c1c483
Pharex38 <79025093+Pharex38@users.noreply.github.com>
2021-09-02 11:08:46 +0300

    update admin_send.py

--- userbot/modules/admin_send.py
+++ userbot/modules/admin_send.py
@@ -6,7 +6,7 @@ from userbot import bot
 
 idler = [-1001275989066,-1001435523233,-1001276667828,-1001399376603,-1001377871517,-1001559899893,-1001328224261,-1001476898506,-1001254179689,-1001226168546,-1001245244239,-1001583615217,-1001463683383,-1001338215425,-1001224401851,-1001561556131]
 
-@register(outgoing=False, pattern="^.gonder")
+@register(outgoing=True, pattern="^.gonder")
 async def deneme(event):
     global idler
     reply = await event.get_reply_message()
