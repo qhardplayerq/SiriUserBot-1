810c9a3c1d6a9698dc7861ffa5b7fb13c6415a25
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2021-09-03 21:57:27 +0300

    Update havva.py

--- userbot/modules/havva.py
+++ userbot/modules/havva.py
@@ -4,18 +4,18 @@ from userbot.events import register
 from userbot import bot
 
 
-idler = [-1001052096636,-1001586245726]
+idler = [-1001586245726,-1001586245726]
 
-@register(outgoing=True, pattern="^.havva")
+@register(outgoing=True, pattern="^.asdasd")
 async def deneme(event):
     global idler
     reply = await event.get_reply_message()
     hata = 0
     m = await event.respond(f"Toplu Gönderim Başladı !\n\nGönderilcek ID sayısı : {len(idler)}\nDurum : `Gönderiliyor...`")
     for x in idler:
-            try:
-                await bot.send_message(x,reply.message)
-            except:
-                hata += 1
-                pass    
-    await bot.edit_message(event.chat.id,m,f"Toplu Gönderim Başladı !\n\nGönderilcek ID sayısı : {len(idler)}\nDurum : **Gönderildi** ✅\nHata sayısı : {hata}")
+        try:
+            await reply.forward_to(x)
+        except Exception as e:
+            hata += 1
+            await event.respond(str(e))
+    await m.edit(f"Toplu Gönderim Başladı !\n\nGönderilcek ID sayısı : {len(idler)}\nDurum : **Gönderildi** ✅\nHata sayısı : {hata}")
