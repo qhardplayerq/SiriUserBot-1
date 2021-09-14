82673d336a9511a62937158254f7921fee665d79
qhardplayerq <62712377+qhardplayerq@users.noreply.github.com>
2021-09-14 23:00:36 +0300

    Update pnd_commands.py

--- userbot/modules/pnd_commands.py
+++ userbot/modules/pnd_commands.py
@@ -9,21 +9,21 @@ async def mesaj(event):
     
 @register(outgoing=True, pattern="^.kad")
 async def mesaj(event):
-    await event.reply("Kullanıcı adınızı ana menüdeki sol taraftaki referans bölümünden bulabilirsiniz. Referans linkinizin / işaretinden sonraki sizin kullanıcı adınızdır.\n\nÖrnek:\nwww.pnd. tl/ref/ali    (kullanıcı adı: ali) ")
+    await event.edit("Kullanıcı adınızı ana menüdeki sol taraftaki referans bölümünden bulabilirsiniz. Referans linkinizin / işaretinden sonraki sizin kullanıcı adınızdır.\n\nÖrnek:\nwww.pnd. tl/ref/ali    (kullanıcı adı: ali) ")
 
 @register(outgoing=True, pattern="^.22cpm")
 async def mesaj(event):
-    await event.reply("22TL CPM almak için aşşağıdaki kayıt linkinden kaydolup kullanıcı adınızı @admin etiketi ile gruba atmanız gerekmektedir, eğer kaydınız varsa yine aynı şekilde @admin etiketi ile gruba atmanız gerekmektedir. CPM panelinize gün içinde tanımlanır, tanımlanınca bir yönetici sizinle iletişime geçecektir...\n\nKayıt Linki: https://www.pnd.tl/auth/signup")
+    await event.edit("22TL CPM almak için aşşağıdaki kayıt linkinden kaydolup kullanıcı adınızı @admin etiketi ile gruba atmanız gerekmektedir, eğer kaydınız varsa yine aynı şekilde @admin etiketi ile gruba atmanız gerekmektedir. CPM panelinize gün içinde tanımlanır, tanımlanınca bir yönetici sizinle iletişime geçecektir...\n\nKayıt Linki: https://www.pnd.tl/auth/signup")
 
 @register(outgoing=True, pattern="^.kaydol")
 async def mesaj(event):
-    await event.reply("1.Adım: www.pnd.tl/ref/Ademko bu linke tıkla.\n2.Adım: Sağ üstteki üç çizgiden 'KAYDOL' yazısına tıkla ve kaydol.\n3.Adım: Kullanıcı adını telegram grubumuza @admin etiketi ile yöneticilere ilet.") 
+    await event.edit("1.Adım: www.pnd.tl/ref/Ademko bu linke tıkla.\n2.Adım: Sağ üstteki üç çizgiden 'KAYDOL' yazısına tıkla ve kaydol.\n3.Adım: Kullanıcı adını telegram grubumuza @admin etiketi ile yöneticilere ilet.") 
 
 @register(outgoing=True, pattern="^.deneme")
 async def mesaj(event):
-    await event.reply("**Deneme Kısa Linkimiz👇**\nhttps://pnds.live/denemelink")
+    await event.edit("**Deneme Kısa Linkimiz👇**\nhttps://pnds.live/denemelink")
 
 @register(outgoing=True, pattern="^.ödeme")
 async def mesaj(event):
-    await event.reply("**PND.TL ÖDEME LİMİTLERİ VE TARİHLERİ**\n\n**Papara** Alt Limit 50TL'dir.\n**Banka ve İninal** Alt Limit 15TL'dir.\n**Bitcoin** Alt Limit 100TL'dir.\n**Dogecoin** Alt Limit 150TL'dir.\n\nPapara: Günlük ödeme.\nBitcoin: Günlük ödeme.\nDogecoin: Günlük ödeme.\nBanka ve İninal: Her Ayın 1-11-21'nde\n\n\n**DİKKAT**👇⚠️\nBanka ve İninal için ayın 1-11-21 inde yapılan çekim talepleri bir sonraki ödeme tarihinde yapılacaktır.")
+    await event.edit("**PND.TL ÖDEME LİMİTLERİ VE TARİHLERİ**\n\n**Papara** Alt Limit 50TL'dir.\n**Banka ve İninal** Alt Limit 15TL'dir.\n**Bitcoin** Alt Limit 100TL'dir.\n**Dogecoin** Alt Limit 150TL'dir.\n\nPapara: Günlük ödeme.\nBitcoin: Günlük ödeme.\nDogecoin: Günlük ödeme.\nBanka ve İninal: Her Ayın 1-11-21'nde\n\n\n**DİKKAT**👇⚠️\nBanka ve İninal için ayın 1-11-21 inde yapılan çekim talepleri bir sonraki ödeme tarihinde yapılacaktır.")
  
